3518ccbfef6ac953aed63733950dc03873e90523
avinash273 <avinash.shanker@mavs.uta.edu>
2020-04-28 00:29:52 -0500

    Graph skeleton

---
+++ graph.py
@@ -0,0 +1,30 @@
+import numpy as np
+import abc
+
+class Graph(abc.ABC):
+    def __init__(self, numVertices, directed=False):
+        self.numVertices = numVertices
+        self.directed = directed
+
+    @abc.abstractmethod
+    def add_edge(self, v1, v2, weight):
+        pass
+
+    @abc.abstractmethod
+    def get_adjacent_vertices(self, v):
+        pass
+
+    @abc.abstractmethod
+    def get_indegree(self, v):
+        pass
+
+    @abc.abstractmethod
+    def display(self):
+        pass
+
+
+class AdjacencyMatrixGraph(Graph):
+
+    def __init__(self, numVertices, directed=False):
+        super(AdjacencyMatrixGraph, self).__init__(numVertices, directed)
+        self.matrix = np.zeros((numVertices, numVertices))
